c9462fd0f69660530dc2124bff207be2f512d81e
nishant <nishantnimbare@gmail.com>
2018-09-21 21:32:08 +0530

    viewProfile fragment

---
+++ app/src/main/java/com/imbuegen/staffapp/fragments/ViewProfileFragment.java
@@ -0,0 +1,33 @@
+package com.imbuegen.staffapp.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.imbuegen.staffapp.R;
+
+public class ViewProfileFragment extends Fragment {
+    ImageView profilePic;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_view_profile,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        profilePic=(ImageView)view.findViewById(R.id.img_profile_pic);
+        profilePic.setImageResource(R.drawable.ic_person_black_24dp);
+
+
+    }
+}
