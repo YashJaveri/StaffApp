410a9babd195ab035e58eba08df98d6ce59ff9bb
YashJaveri <yjaveri99@gmail.com>
2018-09-22 06:38:14 +0530

    FCM DONE

--- app/src/main/java/com/imbuegen/staffapp/services/FirebaseService.java
+++ app/src/main/java/com/imbuegen/staffapp/services/FirebaseService.java
@@ -9,6 +9,9 @@ import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.imbuegen.staffapp.Constants;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -17,36 +20,29 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 
+import cz.msebera.android.httpclient.Header;
+
 public class FirebaseService extends FirebaseInstanceIdService {
     @Override
     public void onTokenRefresh() {
 
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         Log.d("FCMSERVICE", "Refreshed token: " + refreshedToken);
-        sendTokenToServerAsync asn = new sendTokenToServerAsync();
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         String UserHash = preferences.getString("HashCode",null);
-        asn.execute(refreshedToken,UserHash);
-    }
-    class sendTokenToServerAsync extends  AsyncTask<String,Void,Void>{
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.addHeader("Authentication","token " + UserHash);
+        client.addHeader("fcm_token",refreshedToken);
+        client.post(Constants.BASE_URL + "/user/fcm", new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
 
-        @Override
-        protected Void doInBackground(String... strings) {
-            try {
-                URL url = new URL("http://tsec-18.herokuapp.com/user/fcm/" + strings[0]);
-
-                HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-
-                if(strings[1] != null){
-                    connection.setRequestMethod("POST");
-                    connection.addRequestProperty("Authorization","token " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjViYTRmZDUyY2VhNmU3NWM4MWFmYzVjYyJ9.6J52iC4770C0fl8BnIvYMXgradbAsRapyw_d-DJT71w");
-                    Log.d("vsdv",strings[0]);
-                }
-                connection.connect();
-            } catch (Exception e) {
-                e.printStackTrace();
             }
-            return null;
-        }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+            }
+        });
     }
 }
