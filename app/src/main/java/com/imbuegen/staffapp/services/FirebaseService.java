860282bdbe0a38093e5f3dabf3e1bf60986efa52
Rushabh Shroff <rshroff98@gmail.com>
2018-09-22 07:23:19 +0530

    awfq3egqegfQEF

--- app/src/main/java/com/imbuegen/staffapp/services/FirebaseService.java
+++ app/src/main/java/com/imbuegen/staffapp/services/FirebaseService.java
@@ -1,6 +1,5 @@
 package com.imbuegen.staffapp.services;
 
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Handler;
@@ -27,10 +26,9 @@ import cz.msebera.android.httpclient.Header;
 public class FirebaseService extends FirebaseInstanceIdService {
     @Override
     public void onTokenRefresh() {
-
+        super.onTokenRefresh();
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         Log.d("FCMSERVICE", "Refreshed token: " + refreshedToken);
-<<<<<<< HEAD
 
     }
     public  void sendTokenToServer(final String token) {
@@ -51,33 +49,5 @@ public class FirebaseService extends FirebaseInstanceIdService {
 
             }
         });
-    }
-=======
-        sendTokenToServerAsync asn = new sendTokenToServerAsync();
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        String UserHash = preferences.getString("HashCode",null);
-        asn.execute(refreshedToken,UserHash);
-    }
-    class sendTokenToServerAsync extends  AsyncTask<String,Void,Void>{
-
-        @Override
-        protected Void doInBackground(String... strings) {
-            try {
-                URL url = new URL("http://tsec-18.herokuapp.com/user/fcm/" + strings[0]);
-
-                HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-
-                if(strings[1] != null){
-                    connection.setRequestMethod("POST");
-                    connection.addRequestProperty("Authorization","token " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjViYTRmZDUyY2VhNmU3NWM4MWFmYzVjYyJ9.6J52iC4770C0fl8BnIvYMXgradbAsRapyw_d-DJT71w");
-                    Log.d("vsdv",strings[0]);
-                }
-                connection.connect();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
->>>>>>> f7c40256e7b7bdde1119a69bcad873ba62427246
     }
 }
